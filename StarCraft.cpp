3d252a5297e33a1dd9b0fce2cb2e7099fca2131d
yongchaekim <37099211+yongchaekim@users.noreply.github.com>
2023-12-28 20:11:43 +0900

    Update StarCraft.cpp

--- StarCraft.cpp
+++ StarCraft.cpp
@@ -1,6 +1,8 @@
-#include <stdio.h>
-#define MAXN (100)
+#include <iostream>
+#include <queue>
+using namespace std;
 
+#define MAXN (100)
 int W, H;
 char map[MAXN+10][MAXN+10];
 int data[MAXN+10][MAXN+10];
@@ -10,96 +12,81 @@ int dr[]={-1, 1, 0, 0};
 int dc[]={0, 0, -1, 1};
 int valid=0;
 
-void InputData(void) {
-	scanf("%d %d", &W, &H);
-	for(int i=1; i<=H; i++) {
-		scanf("%s", &map[i][1]);
+void InputData() {
+	cin >> W >> H;
+	for(int i; i<=H; i++) {
+		cin >> &map[i][1];
 	}
-	scanf("%d %d", &sw, &sh);
 	
 	for(int i=1; i<=H; i++) {
 		for(int j=1; j<=W; j++) {
-			data[i][j] = map[i][j]-'0';
+			data[i][j] = int(map[i][j]);
 		}
 	}
 }
 
 int check_boundary(int r, int c) {
-	if((r > 0 && r <= H) && (c > 0 && c <= W))
-		return 1;
+	if((r>0 && r<=H) && (c>0 && c<=W)) return 1;
 	return 0;
 }
 
-int Clean(void) {	
+int Clean() {
 	int cnt=0;
 	for(int i=1; i<=H; i++) {
-		for(int j=1; j<=W; j++) {
+		for(int j=1; j<=W; j++){
 			if(data[i][j] == 2) {
 				data[i][j] = 3;
-			} else if(data[i][j] == 1) {
+			} else if (data[i][j]==1){
 				cnt++;
 			}
 		}
 	}
-	return cnt;	
+	return cnt;
 }
 
 void FloodFill(int r, int c) {
-
 	if(!check_boundary(r, c)) return;
-	if(data[r][c]==1) {
-		data[r][c] =2;
+	if(data[r][c]=1) {
+		data[r][c]=2;
 		valid=1;
 		return;
 	}
-	if(data[r][c] != 3) return;
-	data[r][c]= 2;
+	
+	if(data[r][c]!=3) return;
+	data[r][c]=2;
 	for(int i=0; i<4; i++) {
 		FloodFill(r+dr[i], c+dc[i]);
 	}
 }
-/*
-void print(void) {
-	for(int i=1; i<=H; i++) {
-		for(int j=1; j<=W; j++) {
-			printf("%d ", data[i][j]);
-		}
-		printf("\n");
-	}
-	
-	printf("\n");
-}
-*/
-void Solve(void) {
+
+void Solve() {
 	sozergling=0;
 	for(int i=1; i<=H; i++) {
 		for(int j=1; j<=W; j++) {
-			if(data[i][j] == 1) 
-				sozergling++; //Count the number of StarCraft 
+			if(data[i][j]==1) {
+				sozergling++;
+			}
 		}
 	}
-	data[sh][sw] = 3;
-//	print();
+	data[sh][sw]=3;
 	soltime=3;
 	for(; ;) {
-		//print();
 		FloodFill(sh, sw);
 		int ret = Clean();
-		sozergling = ret;
-		
-		if(valid == 0) break;
+		sozergling=ret;
+		if(valid==0) break;
 		soltime++;
-		if(ret == 0) break;
+		if(ret==0) break;
 		valid=0;
-		
 	}
-	//print();
 }
 
-
-int main(void) {
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 	InputData();
 	Solve();
-	printf("%d\n%d\n", soltime, sozergling);
-	return 0;
+	cout << soltime << "\n";
+	cout << sozergling << "\n";
 }
